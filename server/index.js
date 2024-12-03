8e31b315359664faa420226965f0750765e2ef07
Punit Mudgal <ssmudgal01@gmail.com>
2024-12-03 15:01:37 +0530

    dirname change

--- server/index.js
+++ server/index.js
@@ -81,7 +81,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../ui/build")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../ui/build", "index.html"));
+    res.sendFile(path.join(__dirname, "../ui", "build", "index.html"));
   });
 }
 
